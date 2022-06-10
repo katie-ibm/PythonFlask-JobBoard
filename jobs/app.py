87cc2c035ab9706271abf86f9a0c2b8217bbab21
Katie Harshman <katie@ibm.com>
2022-06-10 11:27:40 -0700

    various sql fixes

--- jobs/app.py
+++ jobs/app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, g, request, redirect, url_for
 import sqlite3
 import datetime
 
-PATH = './db/jobs.sqlite'
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 def open_connection():
-    connection = getattr(g, '_connetion', None)
+    connection = getattr(g, '_connection', None)
     if connection == None:
         connection = sqlite3.connect(PATH)
         g._connection = sqlite3.connect(PATH)
@@ -20,13 +20,13 @@ def execute_sql(sql, values=(), commit=False, single=False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
-        cursor.close()
+    cursor.close()
     return results
 
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
-    if connection != None:
+    if connection is not None:
         connection.close()
 
 @app.route('/')
@@ -47,14 +47,17 @@ def employer(employer_id):
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
 
-@app.route('/employer/<employer_id>/review', methods=['GET', 'POST'])
+@app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
 def review(employer_id):
     if request.method == 'POST':
         review = request.form['review']
         rating = request.form['rating']
         title = request.form['title']
         status = request.form['status']
+
         date = datetime.datetime.now().strftime('%m/%d/%Y')
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+        
         return redirect(url_for('employer', employer_id=employer_id))
+        
     return render_template('review.html', employer_id=employer_id)
